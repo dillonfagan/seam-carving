f7cbcdbb863467fab473612a8eb4b4a923f03c6e
Dillon Fagan <dillonfagan@gmail.com>
2017-11-19 11:57:06 -0500

    Improvements to dynamic programming

--- resizeable_image.py
+++ resizeable_image.py
@@ -13,11 +13,56 @@ class ResizeableImage(imagematrix.ImageMatrix):
 
         return seam
 
-    def naive(self):
+    def dynamic(self):
+        max_x = self.width - 1
+        max_y = self.height - 1
+        memo = {}
+        prev = {}
+        start = 0
+
+        # first row of pixels (j = 0)
+        for i in range(0, max_x + 1):
+            memo[i, 0] = self.energy(i, 0)
+
+        # remaining rows
+        for j in range(1, max_y + 1):
+            for i in range(0, max_x + 1):
+                lowest = (i, j-1)
+
+                try:
+                    if memo[i-1, j-1] < memo[lowest]:
+                        lowest = (i-1, j-1)
+                except KeyError:
+                    pass
+
+                try:
+                    if memo[i+1, j-1] < memo[lowest]:
+                        lowest = (i+1, j-1)
+                except KeyError:
+                    pass
+
+                memo[i, j] = memo[lowest] + self.energy(i, j)
+                prev[i, j] = lowest
+
+                if j == max_y:
+                    if memo[i, max_y] < memo[start, max_y]:
+                        start = i
+
+        # climb up matrix to assemble seam
         seam = []
+        j = max_y
+        i = start # start from pixel in last row with min energy
+        while j >= 0:
+            seam.append((i, j))
+            if j == 0:
+                break
+
+            # determine next pixel in seam
+            i, j = prev[i, j]
+
         return seam
 
-    def dynamic(self):
+    def naive(self):
         max_x = self.width - 1
         max_y = self.height - 1
         memo = {}
