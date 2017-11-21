b6a2583ab85cd1201cccc180ed98c77308daf61a
Dillon Fagan <dillonfagan@gmail.com>
2017-11-20 21:33:01 -0500

    Naïve implementation
    (Somewhat) proper implementation of the naïve algorithm is now included. Still returns many seams rather than the single cheapest seam.

--- resizeable_image.py
+++ resizeable_image.py
@@ -1,7 +1,8 @@
 import imagematrix
+from time import time
 
 class ResizeableImage(imagematrix.ImageMatrix):
-    def best_seam(self, dp=True):
+    def best_seam(self, dp = True):
         seam = None
 
         if not dp:
@@ -63,62 +64,44 @@ class ResizeableImage(imagematrix.ImageMatrix):
         return seam
 
     def naive(self):
-        max_x = self.width - 1
-        max_y = self.height - 1
-        memo = {}
+        def recurse(i, j, memo = {}, seam = []):
+            if (i, j) not in memo:
+                memo[i, j] = self.energy(i, j)
 
-        # first row of pixels (j = 0)
-        for i in range(0, max_x + 1):
-            memo[i, 0] = self.energy(i, 0)
-
-        # remaining rows
-        for j in range(1, max_y + 1):
-            for i in range(0, max_x + 1):
-                p = []
-                p.append(memo[i, j-1])
-
-                try:
-                    p.append(memo[i-1, j-1])
-                except KeyError:
-                    pass
-
-                try:
-                    p.append(memo[i+1, j-1])
-                except KeyError:
-                    pass
-
-                memo[i, j] = min(x for x in p) + self.energy(i, j)
-
-        start = 0
-        for i in range(0, max_x + 1):
-            if memo[i, max_y] < memo[start, max_y]:
-                start = i
-
-        # climb up matrix to assemble seam
-        seam = []
-        j = max_y
-        i = start # start from pixel in last row with min energy
-        while j >= 0:
-            seam.append((i, j))
             if j == 0:
-                break
+                return seam
+            else:
+                p = []
 
-            j -= 1
+                p.append((self.energy(i, j-1), (i, j-1)))
 
-            # determine next pixel in seam
-            a = memo[i, j] # up
-            b = None
-            c = None
+                try:
+                    p.append((self.energy(i-1, j-1), (i-1, j-1)))
+                except Exception:
+                    pass
 
-            if i > 0:
-                b = memo[i-1, j] # up left
-            if i < max_x:
-                c = memo[i+1, j] # up right
+                try:
+                    p.append((self.energy(i+1, j-1), (i+1, j-1)))
+                except Exception:
+                    pass
 
-            if b and b < a and b < c:
-                i -= 1
-            elif c and c < a and c < b:
-                i += 1
+                lowest = min(p, key = lambda x: x[0])
+                seam.append(lowest[1])
+
+                return recurse(lowest[1][0], j-1, memo, seam)
+
+        memo = {}
+        seams = []
+        i = 0
+        while i < self.width:
+            s = recurse(i, self.height - 1, memo)
+            seams.append(s)
+            i += 1
+
+        seam = seams[0]
+        for s in seams:
+            if memo[s[0]] < memo[seam[0]]:
+                seam = s
 
         return seam
 
